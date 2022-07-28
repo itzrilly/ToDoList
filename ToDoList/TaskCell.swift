4121b6e3600ca5c0ce5ecd3cee8c9fd8e451e0f9
itzrilly <rilwanougarbarilly@gmail.com>
2022-07-28 10:47:48 +0100

    Définir l'état d'une vue avec les sources de vérité: Transmettre des données via les paramètres d'une vue

---
+++ ToDoList/TaskCell.swift
@@ -0,0 +1,55 @@
+//
+//  TaskCell.swift
+//  ToDoList
+//
+//  Created by RILLY on 28/07/2022.
+//
+
+import SwiftUI
+
+enum Priority {
+    case hight
+    case normal
+    case low
+}
+
+struct TaskCell: View {
+    
+    let name: String
+    let priority: Priority
+    let isDone: Bool
+    
+    private var flagColor: Color {
+        switch priority {
+            case .hight:
+                return .red
+            case .normal:
+                return .orange
+            case .low:
+                return .blue
+        }
+    }
+    
+    var body: some View {
+        HStack {
+            VStack (alignment: .leading, spacing: 8) {
+                Text(name)
+                Image(systemName: "flag.fill")
+                    .foregroundColor(flagColor)
+            }
+            Spacer()
+            Image(systemName: isDone ? "circle.inset.filled" : "circle")
+        }
+        .padding()
+    }
+}
+
+struct TaskCellView_Previews: PreviewProvider {
+    static var previews: some View {
+        Group {
+            TaskCell(name: "Preview task", priority: .normal, isDone: false)
+            TaskCell(name: "Preview task", priority: .hight, isDone: true)
+        }
+        .previewLayout(.sizeThatFits)
+    }
+}
