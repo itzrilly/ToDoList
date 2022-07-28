cbb50149bf98fbedcb31b2a79b3c032de1a80550
itzrilly <rilwanougarbarilly@gmail.com>
2022-07-28 10:14:46 +0100

    Initial Commit

---
+++ ToDoList/ToDoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoListApp.swift
+//  ToDoList
+//
+//  Created by RILLY on 28/07/2022.
+//
+
+import SwiftUI
+
+@main
+struct ToDoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
